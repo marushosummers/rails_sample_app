8fc45acf70e1904b19e1ddc6046693078cdff710
Sho Maruyama <mrsho34@gmail.com>
2018-10-10 20:44:48 +0900

    chage root

--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,6 @@
 Rails.application.routes.draw do
+  root "application#home"
+
   get 'static_pages/Foo'
   get 'static_pages/about'
   get 'static_pages/home'
@@ -7,5 +9,4 @@ Rails.application.routes.draw do
   get 'static_pages/help'
 
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
-  root "application#hello"
 end
