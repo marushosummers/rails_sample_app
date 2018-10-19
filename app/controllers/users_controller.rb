26b559530084c33c72333e844bfa92054f8e50b6
Sho Maruyama <mrsho34@gmail.com>
2018-10-19 11:51:09 +0900

    redirect user

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -10,7 +10,7 @@ class UsersController < ApplicationController
   def create
     @user = User.new(user_params)
     if @user.save
-      # 保存の成功をここで扱う。
+      redirect_to @user
     else
       render 'new'
     end
@@ -22,5 +22,5 @@ class UsersController < ApplicationController
       params.require(:user).permit(:name, :email, :password,
                                    :password_confirmation)
     end
-    
+
 end
