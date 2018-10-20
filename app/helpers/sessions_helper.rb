3c6c21228aa6202b216a37d2e12f57ebf8cf2d2d
Sho Maruyama <mrsho34@gmail.com>
2018-10-20 23:36:19 +0900

    logout

--- app/helpers/sessions_helper.rb
+++ app/helpers/sessions_helper.rb
@@ -20,15 +20,22 @@ module SessionsHelper
     !current_user.nil?
   end
 
+  def forget(user)
+    user.forget
+    cookies.delete(:user_id)
+    cookies.delete(:remember_token)
+  end
+
   def log_out
-  session.delete(:user_id)
-  @current_user = nil
+    forget(current_user)
+    session.delete(:user_id)
+    @current_user = nil
   end
 
   def remember(user)
-  user.remember
-  cookies.permanent.signed[:user_id] = user.id
-  cookies.permanent[:remember_token] = user.remember_token
+    user.remember
+    cookies.permanent.signed[:user_id] = user.id
+    cookies.permanent[:remember_token] = user.remember_token
   end
 
 end
