7a0bb6d46d54da088c3a243cc6250cf552d9bcb7
Sho Maruyama <mrsho34@gmail.com>
2018-10-20 23:47:33 +0900

    bug

--- test/models/user_test.rb
+++ test/models/user_test.rb
@@ -47,7 +47,7 @@ class UserTest < ActiveSupport::TestCase
     @user.save
     assert_equal mixed_case_email.downcase, @user.reload.email
   end
-  
+
   test "password should be present (nonblank)" do
     @user.password = @user.password_confirmation = " " * 6
     assert_not @user.valid?
@@ -58,4 +58,8 @@ class UserTest < ActiveSupport::TestCase
     assert_not @user.valid?
   end
 
+  test "authenticated? should return false for a user with nil digest" do
+    assert_not @user.authenticated?('')
+  end
+  
 end
