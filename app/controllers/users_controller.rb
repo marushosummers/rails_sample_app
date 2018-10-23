0399bc89de87e9e1e2bf6603bfac659d3f0c5768
Sho Maruyama <mrsho34@gmail.com>
2018-10-23 21:08:54 +0900

    log in user only

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,4 +1,13 @@
 class UsersController < ApplicationController
+  before_action :logged_in_user, only: [:edit, :update]
+
+  def logged_in_user
+    unless logged_in?
+      flash[:danger] = "Please log in."
+      redirect_to login_url
+    end
+  end
+
   def new
     @user = User.new
   end
