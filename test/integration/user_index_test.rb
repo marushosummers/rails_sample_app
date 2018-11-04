5d7452de7abb37099ac5bbf62e3ddf9af79ee151
Sho Maruyama <mrsho34@gmail.com>
2018-11-05 00:52:37 +0900

    add test

--- test/integration/user_index_test.rb
+++ test/integration/user_index_test.rb
@@ -1,7 +1,23 @@
 require 'test_helper'
 
 class UserIndexTest < ActionDispatch::IntegrationTest
-  # test "the truth" do
-  #   assert true
-  # end
+
+  require 'test_helper'
+
+  class UsersIndexTest < ActionDispatch::IntegrationTest
+    def setup
+      @user = users(:michael)
+    end
+
+    test "index including pagination" do
+      log_in_as(@user)
+      get users_path
+      assert_template 'users/index'
+      assert_select 'div.pagination'
+      User.paginate(page: 1).each do |user|
+        assert_select 'a[href=?]', user_path(user), text: user.name
+      end
+    end
+  end
+
 end
