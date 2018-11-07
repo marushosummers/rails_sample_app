25627a5b9b7488cbd060b68f902bdd2e53fa61e8
Sho Maruyama <mrsho34@gmail.com>
2018-11-07 18:26:32 +0900

    add password reset views and some action

--- app/controllers/password_resets_controller.rb
+++ app/controllers/password_resets_controller.rb
@@ -1,7 +1,7 @@
 class PasswordResetsController < ApplicationController
   def new
   end
-  
+
   def create
      @user = User.find_by(email: params[:password_reset][:email].downcase)
      if @user
@@ -17,4 +17,17 @@ class PasswordResetsController < ApplicationController
 
   def edit
   end
+
+  private
+
+  def get_user
+    @user = User.find_by(email: params[:email])
+  end
+
+  def valid_user
+    unless (@user && @user.activated? && @user.authenticated?(:reset, params[:id]))
+      redirect_to root_url
+    end
+  end
+
 end
