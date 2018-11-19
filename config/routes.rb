26c5146b205299b24cec4fd1a28c77fa47cd73ce
Sho Maruyama <mrsho34@gmail.com>
2018-11-19 16:43:52 +0900

    follow btn

--- config/routes.rb
+++ config/routes.rb
@@ -21,9 +21,10 @@ Rails.application.routes.draw do
       get :following, :followers
     end
   end
-  
+
   resources :users
   resources :account_activations, only: [:edit]
   resources :password_resets,     only: [:new, :create, :edit, :update]
   resources :microposts,          only: [:create, :destroy]
+  resources :relationships,       only: [:create, :destroy]
 end
